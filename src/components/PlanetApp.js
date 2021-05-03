7509245f5e26c1b60b3afed0aadd6eba3852bc1f
CryptoThibault <thibault.chalaoux@gmail.com>
2021-05-03 12:33:08 +0200

    end

--- src/components/PlanetApp.js
+++ src/components/PlanetApp.js
@@ -6,7 +6,10 @@ const PlanetApp = () => {
   const [planets, setPlanets] = useState([])
   const [page, setPage] = useState(1)
   const [loading, setLoading] = useState(true)
+  const [next, setNext] = useState(null)
+
   useEffect(() => {
+    setLoading(true)
     const url = `https://swapi.dev/api/planets/?page=${page}`
     fetch(url)
       .then((response) => {
@@ -18,18 +21,19 @@ const PlanetApp = () => {
       .then((data) => {
         setPlanets(planets => [...planets, ...data.results])
         setLoading(false)
+        setNext(!!data.next)
       })
   }, [page])
 
   return (
-    <div className="container py-5">
-      <h1 className="mb-5">Planètes dans l'univers star wars</h1>
-      <div className="row" >
+    <div className=" py-5">
+      <div className="container" >
+        <h1 className="mb-5">Planètes dans l'univers star wars</h1>
         {loading && (
           <div className="mb-4 text-center p-3">loading...</div>
         )}
         <Planets planets={planets} />
-        <Scroll page={page} setPage={setPage} />
+        <Scroll page={page} setPage={setPage} next={next} />
       </div>
     </div>
   );
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,5 +1,5 @@
-const Scroll = ({ page, setPage }) => {
-  return page !== 3
+const Scroll = ({ page, setPage, next }) => {
+  return next
     ? (<button className="btn btn-dark" onClick={() => setPage(page + 1)}>Suivantes</button>)
     : (<div className="bg-dark text-white p-3">All the planets has been listed.</div>)
 
